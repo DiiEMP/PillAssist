038dd746d0034a6cceb724e5ab52fac97a1fbc58
Diana Milla <123897667+DiiEMP@users.noreply.github.com>
2023-10-30 22:47:42 -0600

    Calendario pestaña

--- app/src/main/java/com/example/pillassist/MainActivity.java
+++ app/src/main/java/com/example/pillassist/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.pillassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button crear, fpswd;
+    Button crear, fpswd, ingresar;
     String valor = "";
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(f);
             }
         });
+
+        ingresar = findViewById(R.id.iniciarSesion);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent is = new Intent(MainActivity.this, Calendario.class);
+                startActivity(is);
+            }
+        });
+
 
     }
 }
