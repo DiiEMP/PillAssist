b98a41acebb3e8e340964c946a8f9277c78c3ae3
migue001 <40450007+migue001@users.noreply.github.com>
2023-11-06 18:08:17 -0600

    Activities Admin

--- app/src/main/java/com/example/pillassist/MainActivity.java
+++ app/src/main/java/com/example/pillassist/MainActivity.java
@@ -2,20 +2,40 @@ package com.example.pillassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Cambios de ventana
-    Button crear, fpswd;
+    Button crear, fpswd, ingresar;
     String valor = "";
+    EditText correo, contrasena;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         crear = findViewById(R.id.crearCuenta);
+        ingresar = findViewById(R.id.ingresar);
+        correo = findViewById(R.id.user);
+        contrasena = findViewById(R.id.contrasena);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(correo.getText().toString().equalsIgnoreCase("admin@gmail.com") && contrasena.getText().toString().equalsIgnoreCase("12345")){
+                    Intent ingresar = new Intent(MainActivity.this, RegisterMed.class);
+                    startActivity(ingresar);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Credenciales Incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         crear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
