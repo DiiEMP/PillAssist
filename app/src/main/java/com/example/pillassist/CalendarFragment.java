b14628b770398da135b1547ea0d360629a8ef41f
Diana Milla <123897667+DiiEMP@users.noreply.github.com>
2023-11-12 15:06:39 -0600

    Fragments Ediciones de xml

--- app/src/main/java/com/example/pillassist/CalendarFragment.java
+++ app/src/main/java/com/example/pillassist/CalendarFragment.java
@@ -1,11 +1,14 @@
 package com.example.pillassist;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.graphics.Color;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
@@ -13,44 +16,51 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class Calendario extends AppCompatActivity {
+public class CalendarFragment extends Fragment {
 
-    CalendarView calendarView;
-    Calendar calendar;
+    private CalendarView calendarView;
+    private Calendar calendar;
+
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendario);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-        calendarView = findViewById(R.id.calendarVw);
+        calendarView = view.findViewById(R.id.calendarVw);
         calendar = Calendar.getInstance();
 
-        // Obtén la fecha actual del sistema
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         int month = calendar.get(Calendar.MONTH);
         int year = calendar.get(Calendar.YEAR);
 
-        //setDate(1, 11, 2023);
         setDate(day, month, year);
         getDate();
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                Toast.makeText(Calendario.this, day + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireContext(), day + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
             }
         });
+
+        return view;
     }
 
-    public void getDate() {
+    private void getDate() {
         long date = calendarView.getDate();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
         calendar.setTimeInMillis(date);
         String selected_date = simpleDateFormat.format(calendar.getTime());
-        Toast.makeText(this, selected_date, Toast.LENGTH_SHORT).show();
+        Toast.makeText(requireContext(), selected_date, Toast.LENGTH_SHORT).show();
     }
 
-    public  void setDate(int day, int month, int year) {
+    private void setDate(int day, int month, int year) {
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.MONTH, month);
         calendar.set(Calendar.DAY_OF_MONTH, day);
