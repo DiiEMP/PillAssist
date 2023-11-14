b7f2b77efdf56dea634fcde3a07e46bc424d0be0
Diana Milla <123897667+DiiEMP@users.noreply.github.com>
2023-11-13 21:52:57 -0600

    BDD Registrar Usuario

--- app/src/main/java/com/example/pillassist/ProfileFragment.java
+++ app/src/main/java/com/example/pillassist/ProfileFragment.java
@@ -1,26 +1,21 @@
 package com.example.pillassist;
-
+import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +23,6 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -58,7 +44,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        // Aquí puedes realizar cualquier operación necesaria en el fragmento
+        // Puedes acceder a elementos de la vista (view) y asignarles clicadores, etc.
+        return view;
+    }
+
+    // Método para manejar el resultado de la selección de la imagen
+    public void handleImageSelectionResult(Uri uri) {
+        // Puedes realizar acciones adicionales aquí con la URI de la imagen seleccionada
     }
 }
--- app/src/main/java/com/example/pillassist/Register.java
+++ app/src/main/java/com/example/pillassist/Register.java
@@ -2,13 +2,85 @@ package com.example.pillassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class Register extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    EditText nombre, email, contrasenia, fechaNacimiento;
+    Button regis, iniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        iniciarConexion();
+
+        nombre = findViewById(R.id.nombre);
+        email = findViewById(R.id.correo);
+        contrasenia = findViewById(R.id.contrasena);
+        fechaNacimiento = findViewById(R.id.fechaNac);
+
+        regis = findViewById(R.id.Registrar);
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrarUser(nombre.getText().toString(), email.getText().toString(), contrasenia.getText().toString(), fechaNacimiento.getText().toString());
+                limpiarDatos();
+            }
+        });
+
+        iniciarSesion = findViewById(R.id.RegresarInicioSesion);
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               finish();
+            }
+        });
+
+
     }
+
+
+    private void iniciarConexion() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    public void registrarUser(String nombreU, String emailU, String contraseniaU, String fechaNacU) {
+        RegisterUser regUsr = new RegisterUser();
+        regUsr.setIdUser(UUID.randomUUID().toString());
+        regUsr.setName(nombreU);
+        regUsr.setEmail(emailU);
+        regUsr.setContrasenia(contraseniaU);
+        regUsr.setFechaNac(fechaNacU);
+        databaseReference.child("Usuarios").child(regUsr.getIdUser()).setValue(regUsr);
+        Toast.makeText(this, "Usuario agregado exitosamente", Toast.LENGTH_SHORT).show();
+    }
+
+    private void limpiarDatos() {
+        nombre.setText("");
+        email.setText("");
+        contrasenia.setText("");
+        fechaNacimiento.setText("");
+    }
+
+
+
 }
+
